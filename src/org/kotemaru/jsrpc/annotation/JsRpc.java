3943c13d753b59024b03d25c9b27b68b1ea5b992
kotemaru <kotemaru@kotemaru.org>
2011-08-01 19:44:12 +0900

    jsproc ドキュメント追加

--- src/org/kotemaru/jsrpc/annotation/JsRpc.java
+++ src/org/kotemaru/jsrpc/annotation/JsRpc.java
@@ -8,7 +8,16 @@ import java.lang.annotation.ElementType;
 import org.kotemaru.apthelper.annotation.ProcessorGenerate;
 
 import org.kotemaru.jsrpc.apt.JavaScriptStubHelper;
+import org.kotemaru.jsrpc.*;
 
+
+
+/**
+{@see CalljavaServlet}クラス用のJavaScriptスタブを生成するアノテーション。
+<li>当該クラスと同じパッケージに "クラス名.js" としてスタブを生成する。
+<li>今の所、public static のメソッドのみが対象。
+@autor kotemaru@kotemaru.org
+*/
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.TYPE)
 @ProcessorGenerate(template="JavaScriptStub.vm",suffix=".js",
